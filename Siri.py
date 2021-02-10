9d6e7efa0afd18d248a3d14c9f8c8bec76b198c8
Apoorva Reddy <61325120+apoorvareddy612@users.noreply.github.com>
2021-02-10 23:40:31 +0530

    Add files via upload

---
+++ Siri.py
@@ -0,0 +1,54 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit as kit
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[0].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+  try:
+      with sr.Microphone() as source:
+          print("Listening...")
+          voice = listener.listen(source)
+          command = listener.recognize_google(voice)
+          command = command.lower()
+
+          print(command)
+
+  except:
+      pass
+
+  return command
+
+def run_siri():
+    command = take_command()
+    if 'play' in command:
+        talk('playing')
+        print('playing')
+        kit.playonyt(command)
+
+    if 'send' in command:
+        talk('sending')
+        print('sending...')
+        kit.sendwhatmsg("+918810519744",command[4:],23,20,1)
+
+    if 'search' in command:
+        talk('searching')
+        print('searching')
+        kit.search(command[6:])
+
+    if 'find about' in command:
+        talk('finding information about {}'.format(command[10:]))
+        print("Finding")
+        kit.info(command[10:], lines = 5)
+
+    else:
+        talk("I don't know what are you talking about")
+
+run_siri()
